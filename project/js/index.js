1e3cb19265b00aae120b655610de3e9baf22b6a4
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2023-12-13 20:58:21 -0500

    yar

--- project/js/index.js
+++ project/js/index.js
@@ -1,7 +1,7 @@
 import {DOMSelectors} from "./DOM";
 import '../css/style.css';
 
-const URL = `https://valorant-api.com/v1/agents`;
+const URL = `https://pokemon-go-api.github.io/pokemon-go-api/api/pokedex.json`;
 //https://gateway.marvel.com/v1/public/comics?ts=1&apikey=81cee0fda2f0bdfd993ee07078a68999&hash=a49c63b1d849e5be3f6690dacaf948f2
 
 async function getData(URL){
@@ -19,10 +19,9 @@ async function getData(URL){
              DOMSelectors.container.insertAdjacentHTML(
                 "beforeend",
                 `<div class = "card">
-                <h3 class="title">${el.displayName}</h3>
-                <img class="cover" src="${el.image}" alt="image">
-                <h4 class="misc">${el.actor}</h4>
-                <h5 class="misc">${el.house}</h5>
+                <h3 class="cardtitle">${el.id}</h3>
+                <h4 class="misc">Primary Type: ${el.primaryType.names.English}</h4>
+                <h5 class="misc">Secondary Type: ${el.primaryType.names.English}</h5>
             </div>`
             ) 
         )
@@ -48,10 +47,9 @@ function addCard(arr){
     arr.forEach((x)=> DOMSelectors.container.insertAdjacentHTML(
         "beforeend",
         `<div class = "card">
-        <h3 class="title">${x.title}</h3>
-        <img src=${x.image} alt="image"> </img>
-        <h4 class="misc">${x.release_date}</h4>
-        <h5 class="misc">${x.running_time}</h5>
+        <h3 class="title">${x.id}</h3>
+        <h4 class="misc">Primary Type: ${el.primaryType.names.English}</h4>
+        <h5 class="misc">Secondary Type: ${el.primaryType.names.English}</h5>
     </div>`
 ))};
 
